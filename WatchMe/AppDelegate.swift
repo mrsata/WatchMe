6a1c9633a7694d511d3ab17603a5e1ba22a81399
MrSATA <liuhanrick@163.com>
2016-04-15 01:59:26 -0500

    Update UI
    new icons, navigation color, tab bar color, status bar color

--- WatchMe/AppDelegate.swift
+++ WatchMe/AppDelegate.swift
@@ -44,6 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
         return true
     }
 
